4e7488a3d7c7f6bc860ffe312afa6d2f4c9d0124
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-27 21:17:26 +0200

    Class formatted and made @RestController.

--- 01_controller/01_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
+++ 01_controller/01_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -1,23 +1,30 @@
 package org.formacio.mvc;
 
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
+@RestController
 public class PersonalController {
 
-	// no toqueu la declaracio de baseDeDades ni el metode getBaseDeDades
-	// 
-	// als metodes que anau afegint, tracteu aquest array com si fos la base de dades
-	//
-	// per exemple, obtenir la persona amb id 1 sera fer baseDeDades.get(1), etc etc 
-	
-	private List<String> baseDeDades = new ArrayList<>(Arrays.asList("Joana","Antonia","Pere"));
-	
-	public List<String> getBaseDeDades() {
-		return baseDeDades;
-	}
+    /**
+     * No toqueu la declaracio de baseDeDades ni el metode getBaseDeDades.
+     * <p>
+     * Els metodes que anau afegint, tracteu aquest array com si fos la base de dades: per exemple, obtenir la persona
+     * amb id 1 sera fer baseDeDades.get(1), etc etc
+     */
 
-	// Poseu a partir d'aqui els vostre metodes
+    /* ---- Properties ---- */
+    private List<String> baseDeDades = new ArrayList<>(Arrays.asList("Joana", "Antonia", "Pere"));
+
+
+    /* ---- Getters ---- */
+    public List<String> getBaseDeDades() {
+        return baseDeDades;
+    }
+
+
+    /* ---- Behaviours ---- */
 }

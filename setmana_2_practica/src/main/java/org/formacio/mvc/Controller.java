ad116f0355780409707709760e006514b1558756
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 18:52:03 +0200

    @ResponseStatus tag above class declaration.

--- setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
@@ -3,11 +3,10 @@ package org.formacio.mvc;
 import org.formacio.repositori.AgendaService;
 import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "ID not in the database")
 @RestController
 public class Controller {
 

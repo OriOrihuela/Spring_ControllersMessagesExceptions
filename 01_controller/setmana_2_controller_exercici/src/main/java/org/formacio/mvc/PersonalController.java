e9960ed71c05a6967ac9d8185133513731f957b3
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 12:10:07 +0200

    Formatted

--- 01_controller/setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
+++ 01_controller/setmana_2_controller_exercici/src/main/java/org/formacio/mvc/PersonalController.java
@@ -7,17 +7,22 @@ import java.util.List;
 
 public class PersonalController {
 
-	// no toqueu la declaracio de baseDeDades ni el metode getBaseDeDades
-	// 
-	// als metodes que anau afegint, tracteu aquest array com si fos la base de dades
-	//
-	// per exemple, obtenir la persona amb id 1 sera fer baseDeDades.get(1), etc etc 
-	
-	private List<String> baseDeDades = new ArrayList<>(Arrays.asList("Joana","Antonia","Pere"));
-	
-	public List<String> getBaseDeDades() {
-		return baseDeDades;
-	}
+    // no toqueu la declaracio de baseDeDades ni el metode getBaseDeDades
+    //
+    // als metodes que anau afegint, tracteu aquest array com si fos la base de dades
+    //
+    // per exemple, obtenir la persona amb id 1 sera fer baseDeDades.get(1), etc etc
 
-	// Poseu a partir d'aqui els vostre metodes
+
+    /* ---- Properties ---- */
+    private List<String> baseDeDades = new ArrayList<>(Arrays.asList("Joana", "Antonia", "Pere"));
+
+
+    /* ---- Getters ---- */
+    public List<String> getBaseDeDades() {
+        return baseDeDades;
+    }
+
+
+    /* ---- Behaviours ---- */
 }

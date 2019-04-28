45d6713894be171fbf8a4e8050101749073462f4
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 17:19:10 +0200

    Formatted

--- 02_format_plantilla/src/main/java/org/formacio/FormatApplication.java
+++ 02_format_plantilla/src/main/java/org/formacio/FormatApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FormatApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FormatApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FormatApplication.class, args);
+    }
 }

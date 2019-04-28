362045a1013ea4b1651d3f7771d9b06f7fde5fc0
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 18:41:57 +0200

    addNewContact() method implemented.

--- setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
@@ -5,6 +5,7 @@ import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -34,6 +35,11 @@ public class Controller {
     @RequestMapping("/contacte/{id}")
     public Persona returnPerson(@PathVariable String id) {
         return getAgendaService().recupera(id);
+    }
+
+    @RequestMapping(value = "/afegir", method = RequestMethod.POST)
+    public void addNewContact(String id, String nom, String telèfon) {
+        getAgendaService().inserta(id, nom, telèfon);
     }
 
 }

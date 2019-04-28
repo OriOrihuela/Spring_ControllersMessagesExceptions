530b075cbd8346ed0a576db1cfe44a848e4b1753
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 18:03:50 +0200

    returnNumberOfContacts() implemented

--- setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
@@ -2,6 +2,7 @@ package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +11,11 @@ public class Controller {
     /* ---- Properties ---- */
     @Autowired
     private AgendaService agendaService = null;
+
+
+    /* ---- Behaviours ---- */
+    @RequestMapping("/nombre")
+    public int returnNumberOfContacts() {
+        return agendaService.nombreContactes();
+    }
 }

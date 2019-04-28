dcba221cb344eee4ca8d5e4f899171d727476adb
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-28 18:12:43 +0200

    getAgendaService() getter and returnPerson() method implemented.

--- setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
+++ setmana_2_practica/src/main/java/org/formacio/mvc/Controller.java
@@ -1,7 +1,9 @@
 package org.formacio.mvc;
 
 import org.formacio.repositori.AgendaService;
+import org.formacio.repositori.Persona;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,14 +15,25 @@ public class Controller {
     private AgendaService agendaService = null;
 
 
+    /* ---- Getters ---- */
+    public AgendaService getAgendaService() {
+        return this.agendaService;
+    }
+
     /* ---- Behaviours ---- */
     @RequestMapping("/nombre")
     public int returnNumberOfContacts() {
-        return agendaService.nombreContactes();
+        return getAgendaService().nombreContactes();
     }
 
     @RequestMapping("/telefon")
     public String returnPersonPhone(String id) {
-        return agendaService.recupera(id).getTelefon();
+        return getAgendaService().recupera(id).getTelefon();
     }
+
+    @RequestMapping("/contacte/{id}")
+    public Persona returnPerson(@PathVariable String id) {
+        return getAgendaService().recupera(id);
+    }
+
 }

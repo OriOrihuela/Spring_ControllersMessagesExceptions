3d58c9c6c3dd538cd481d2ab647e3f9336595cc9
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-04-30 20:39:34 +0200

    @ResponseStatus above class declaration; @ExceptionHandler above error()

--- setmana_2_practica/src/main/java/org/formacio/errorHandler/ErrorHandler.java
+++ setmana_2_practica/src/main/java/org/formacio/errorHandler/ErrorHandler.java
@@ -1,4 +1,13 @@
 package org.formacio.errorHandler;
 
-public class ErrorHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "ID not in database")
+public class ErrorHandler extends RuntimeException {
+
+    @ExceptionHandler(Exception.class)
+    public void error() {
+    }
 }
